12b833d05bc418bbcc6f72e5c629f690e00bbd00
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-16 15:27:45 -0500

    feat(main-view): implemented main view with in-app navigation.

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,45 @@
 <script>
-
+  export default {
+    name: 'app',
+    title: 'ACME Learning Center',
+    data() {
+      return {
+        drawer: false,
+        items: [
+          { label: 'Home', to: '/home'},
+          { label: 'About', to: '/about'}
+        ],
+      }
+    },
+    methods: {
+      toggleDrawer() {
+        this.drawer = !this.drawer;
+      }
+    }
+  }
 </script>
 
 <template>
-
+  <pv-toast/>
+  <header>
+    <pv-toolbar class="bg-primary">
+      <template #start>
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer"/>
+        <h3>ACME Learning Center</h3>
+        <div class="spacer"></div>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer"/>
+  <router-view/>
 </template>
+<style scoped>
+  .spacer {
+    flex: 1 1 auto;
+  }
+</style>
