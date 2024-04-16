676063bba6ca65e6e905d660688c9b38d501c420
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-16 14:00:08 -0500

    chore: configured development environment.

---
+++ src/shared/services/http-common.js
@@ -0,0 +1,14 @@
+/**
+ * Axios default configuration
+ */
+
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const http = axios.create({
+    baseURL: API_BASE_URL,
+    headers: { 'Content-type': 'application/json'}
+});
+
+export default http;
