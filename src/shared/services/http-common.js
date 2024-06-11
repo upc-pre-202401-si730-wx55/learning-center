1a1ca28830777a141f7a1a99b676deaf09597ad5
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-11 15:06:14 -0500

    feat(cors): cors parameter to default request header.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -8,7 +8,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({
     baseURL: API_BASE_URL,
-    headers: { 'Content-type': 'application/json'}
+    headers: { 'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 });
 
 export default http;
