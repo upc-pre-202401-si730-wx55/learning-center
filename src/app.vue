60683b32d8d4fb85fdc4ce9de52fc9940bca18a3
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-11 14:46:37 -0500

    feat(iam): placed authentication section in main view.

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,9 @@
 <script>
+  import AuthenticationSection from "./iam/components/authentication-section.component.vue";
+
   export default {
     name: 'app',
+    components: {AuthenticationSection},
     title: 'ACME Learning Center',
     data() {
       return {
@@ -27,13 +30,14 @@
       <template #start>
         <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer"/>
         <h3>ACME Learning Center</h3>
-      </template>
-      <template #end>
         <div class="flex-column">
           <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
             <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
           </router-link>
         </div>
+      </template>
+      <template #end>
+        <authentication-section/>
       </template>
     </pv-toolbar>
   </header>
