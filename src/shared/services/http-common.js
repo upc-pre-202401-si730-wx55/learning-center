6b6475762744618eb324db1f53068f3f128c92e8
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-14 07:28:12 -0500

    fix: rectified incorrect location of headers for default request.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -6,9 +6,18 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+/**
+ * Axios instance
+ * @summary http axios instance with default configuration
+ *
+ * @type {AxiosInstance}
+ */
 const http = axios.create({
     baseURL: API_BASE_URL,
-    headers: { 'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 });
+
+// Set default headers
+http.defaults.headers.common['Content-Type'] = 'application/json';
+http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 export default http;
